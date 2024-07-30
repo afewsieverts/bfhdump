8ea1d6e53f6cbb091a6b3a8be7a652c3e225dcb3
Luca <3.5roentgen@gmail.com>
2024-07-30 08:49:31 +0100

    Added side ASCII display to main.asm. Slightly modified makefile, executable now called bfh

--- main.asm
+++ main.asm
@@ -4,7 +4,8 @@ section .bss
     buffer: resb BUFFLEN
 
 section .data
-    hexstr: db "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00", 10
+    hexstr: db " 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00   " 
+    chrstr: db "................", 10
     HEXLEN: equ $-hexstr
 
     DIGITS: db "0123456789ABCDEF"
@@ -22,20 +23,29 @@ READ:
     mov rsi, buffer
     mov rdx, BUFFLEN
     syscall
-    
+
     cmp rax, 1
     jb QUIT
 
     mov r15, rax        ; len of read chars
     mov rsi, buffer     ; *buffer[0]
     mov rdi, hexstr     ; *hexstr[0]
+    inc rdi             ; *buffer[1] as [0] is ' ' for readability
     xor r8, r8
     mov rbx, DIGITS 
+    mov r9, chrstr
     xor rcx, rcx
     xor rax, rax
 
 SCAN:
     mov al, byte [rsi]
+    cmp al, 20h
+    jb HEXDUMP
+    cmp al, 7Eh
+    ja HEXDUMP
+    mov byte [r9], al
+
+HEXDUMP:
     mov cl, al
     shr al, 4 
     shl cl, 4
@@ -51,22 +61,31 @@ SCAN:
     sub rbx, rcx
     inc rdi
     inc rdi
+    inc r9
     inc rsi
 
    ; sub rbx, rax
    ; add rbx, rcx
 
-   inc r8
-   cmp r8, r15
-   jne SCAN
+    inc r8
+    cmp r8, r15
+    jne SCAN
     
-   mov rax, 1
-   mov rdi, rax
-   mov rsi, hexstr
-   mov rdx, HEXLEN
-   syscall
-    
-   jmp READ
+    mov rax, 1
+    mov rdi, rax
+    mov rsi, hexstr
+    mov rdx, HEXLEN
+    syscall
+
+    xor rax, rax
+CLEAN:
+    mov r9, chrstr
+    mov byte [r9 + rax], 2Eh ; reset to '.'
+    inc rax
+    cmp rax, r15
+    jne CLEAN
+
+    jmp READ
 
 QUIT:
     mov rax, 60
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 bfh: main.o
-	ld -o exe main.o
+	ld -o bfh main.o
 
 main.o: main.asm
 	nasm -f elf64 -g -F dwarf main.asm
